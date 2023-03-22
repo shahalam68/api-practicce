4a182711f654b005ba7a6913863a593b03280c36
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-03-23 00:48:21 +0600

    randomt

--- js/random.js
+++ js/random.js
@@ -7,13 +7,27 @@ const getUser = () =>{
 getUser()
 
 
-const displayUser = users =>{
-    users.forEach(user => {
-        const userDiv = document.getElementById('test');
-    const div = document.createElement('div');
-    div.innerHTML =`
-    <h2>${user.name}</h2>
-    `
-    userDiv.appendChild(div)
+
+const displayUser = data =>{
+    const users = data.results;
+    const userDiv = document.getElementById("random-user");
+    users.forEach( user => {
+        console.log(user)
+        const div = document.createElement('div');
+        div.classList.add('card"');
+        div.innerHTML = `
+        <img src="${user.picture.large}" class="card-img-top rounded" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Name: ${user.name.title}</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+        `
+        userDiv.appendChild(div)
+        
     });
 }
+
+
