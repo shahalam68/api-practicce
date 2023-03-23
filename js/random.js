e642fb4c5ea2996385670b75b1f28f3d4ae470d5
Soykot Alam <a.k.mshahalam68@gmail.com>
2023-03-23 22:11:44 +0600

    random user

--- js/random.js
+++ js/random.js
@@ -9,25 +9,25 @@ getUser()
 
 
 const displayUser = data =>{
-    const users = data.results;
-    const userDiv = document.getElementById("random-user");
-    users.forEach( user => {
-        console.log(user)
-        const div = document.createElement('div');
-        div.classList.add('card"');
-        div.innerHTML = `
-        <img src="${user.picture.large}" class="card-img-top rounded" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Name: ${user.name.title}</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        <div class="card-footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
-        </div>
-        `
-        userDiv.appendChild(div)
-        
-    });
+  const users = data.results;
+  const userDiv = document.getElementById("random-user");
+  users.forEach( user => {
+      console.log(user)
+      const div = document.createElement('div');
+      div.classList.add('card"');
+      div.innerHTML = `
+      <img src="${user.picture.large}" class="card-img-top rounded" alt="...">
+      <div class="card-body">
+        <h4 class="card-title">Name: ${user.name.title} ${user.name.first} ${user.name.last}</h4>
+        <h5 class="card-title">Age: ${user.dob.age}</h5>
+        <p class="card-text">Email: ${user.email}</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">Location: ${user.location.street.number} , ${user.location.street.name} </small>
+      </div>
+      `
+      userDiv.appendChild(div)
+      
+  });
 }
-
 
